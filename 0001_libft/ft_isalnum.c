23071f58f5d9568f41d7456664f98e6110c7d2d9
asilva-o <98337913+Aleksaso@users.noreply.github.com>
2023-04-24 21:37:04 +0200

    Update ft_isalnum.c

--- 0001_libft/ft_isalnum.c
+++ 0001_libft/ft_isalnum.c
@@ -10,27 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
+#include "libft.h"
+
 
 int	ft_isalnum(char *str)
 
 {
-	int	i;
 
-	i = 0;
-	while ((str[i] != '\0'))
-	{
-		if ((str[i] >= 32 && str[i] <= 126))
-			i++;
-		else
-			return (0);
-	}
+	
+	
 	return (1);
 }
 
 int	main(void)
 {
-	printf("%i", ft_isalnum("mlg"));
+	
 	return (0);
 }
